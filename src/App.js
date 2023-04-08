4ccdee4b003ebbccbc9e7213456dc4468a2ea8d0
fuad-crypto <fuad.ashadi12@gmail.com>
2023-04-08 22:37:05 +0700

    Finishing component header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Switch , Route } from 'react-router-dom';
+
 import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
-    return <div className="App">Hello World!</div>;
+    return (
+        <div className="App">
+            <Switch>
+                <Route path="/" component={LandingPage}></Route>    
+            </Switch>            
+        </div>
+    );
 }
 
 export default App;
